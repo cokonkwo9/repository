f5c5c44f2cdd6da55df93bab0ff99d67941f5be5
Omega Haileyesus <omegahaile@gmail.com>
2017-02-14 17:47:03 -0500

    added logout button

--- CleanWaterCrowdsourcing/app/src/main/java/com/example/humans/cleanwatercrowdsourcing/SuccessfulLogin.java
+++ CleanWaterCrowdsourcing/app/src/main/java/com/example/humans/cleanwatercrowdsourcing/SuccessfulLogin.java
@@ -1,7 +1,9 @@
 package com.example.humans.cleanwatercrowdsourcing;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by omegahaileyesus on 2/14/17.
@@ -13,4 +15,17 @@ public class SuccessfulLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.successful_login);
     }
+
+
+    public void onLogout(View v) {
+
+        if (v.getId() == R.id.Blogout) {
+
+            Intent login = new Intent(SuccessfulLogin.this, HomeScreen.class);
+            startActivity(login);
+
+        }
+
+    }
 }
+
