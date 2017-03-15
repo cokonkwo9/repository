a66f8eb90b4523e3553649f7aa14f06aab929e25
Omega Haileyesus <omegahaile@gmail.com>
2017-03-14 22:39:54 -0400

    map opens

--- CleanWaterCrowdsourcing/app/src/main/java/com/example/humans/cleanwatercrowdsourcing/ViewMap.java
+++ CleanWaterCrowdsourcing/app/src/main/java/com/example/humans/cleanwatercrowdsourcing/ViewMap.java
@@ -3,14 +3,28 @@ package com.example.humans.cleanwatercrowdsourcing;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 /**
  * Created by omegahaileyesus on 3/14/17.
  */
 
-public class ViewMap extends AppCompatActivity{
+public class ViewMap extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mGoogleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment2);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        mGoogleMap = googleMap;
+        
     }
 }
